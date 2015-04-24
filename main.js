61751e53199018c2ef1c7ff4e1fa676bfd423a6c
romovs <romovs@gmail.com>
2015-04-24 22:00:53 +0300

    Formatting and window title

--- main.js
+++ main.js
@@ -14,56 +14,58 @@ app.on('window-all-closed', function() {
     app.quit();
 });
 
-// This method will be called when atom-shell has done everything
-// initialization and ready for creating browser windows.
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  mainWindow.focus();
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-    menu = null;
-  });
+    mainWindow = new BrowserWindow({
+        width: 800, 
+        height: 600, 
+        title: app.getName() + ' ' + app.getVersion() 
+    });
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.focus();
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+        menu = null;
+    });
   
-  var template = [
-  {
-    label: '&File',
-    submenu: [
-      {
-        label: '&Open',
-        accelerator: 'Ctrl+O',
-        click: function() { openFile(); }
-      },
-      {
-        label: '&Close',
-        accelerator: 'Ctrl+W',
-        click: function() { mainWindow.close(); }
-      },
-    ]
-  },
-  {
-    label: '&View',
-    submenu: [
-      {
-        label: '&Reload',
-        accelerator: 'Ctrl+R',
-        click: function() { mainWindow.restart(); }
-      },
-      {
-        label: '&Toggle DevTools',
-        accelerator: 'Alt+Ctrl+I',
-        click: function() { mainWindow.toggleDevTools(); }
-      },
-    ]
-  },
-  ];
+    var template = [
+    {
+        label: '&File',
+        submenu: [
+            {
+                label: '&Open',
+                accelerator: 'Ctrl+O',
+                click: function() { openFile(); }
+            },
+            {
+                label: '&Close',
+                accelerator: 'Ctrl+W',
+                click: function() { mainWindow.close(); }
+            },
+        ]
+    },
+    {
+        label: '&View',
+        submenu: [
+            {
+                label: '&Reload',
+                accelerator: 'Ctrl+R',
+                click: function() { mainWindow.restart(); }
+            },
+            {
+                label: '&Toggle DevTools',
+                accelerator: 'Alt+Ctrl+I',
+                click: function() { mainWindow.toggleDevTools(); }
+            },
+        ]
+    },
+    ];
 
-  menu = Menu.buildFromTemplate(template);
-  mainWindow.setMenu(menu);
+    menu = Menu.buildFromTemplate(template);
+    mainWindow.setMenu(menu);
 });
 
 function openFile() {
