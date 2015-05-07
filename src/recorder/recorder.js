b753f6028193e3bd613ae7cdf5abf5e763f238e9
romovs <romovs@gmail.com>
2015-05-07 20:29:38 +0300

    Cleaned up \t

--- src/recorder/recorder.js
+++ src/recorder/recorder.js
@@ -37,87 +37,87 @@ Recorder.cmdSend = function (command, target, value, timestamp) {
 };
 
 Recorder.prototype.reattachWindowMethods = function() {
-	if (!this.windowMethods) {
-		this.originalOpen = window.open;
-	}
-	this.windowMethods = {};
-	['alert', 'confirm', 'prompt', 'open'].forEach(function(method) {
-			this.windowMethods[method] = window[method];
-		}, this);
-	var self = this;
-	window.alert = function(alert) {
-	    self.windowMethods.alert.call(self.window, alert);
+    if (!this.windowMethods) {
+        this.originalOpen = window.open;
+    }
+    this.windowMethods = {};
+    ['alert', 'confirm', 'prompt', 'open'].forEach(function(method) {
+            this.windowMethods[method] = window[method];
+        }, this);
+    var self = this;
+    window.alert = function(alert) {
+        self.windowMethods.alert.call(self.window, alert);
         self.record('assertAlert', alert);
-	};
-	window.confirm = function(message) {
-		var result = self.windowMethods.confirm.call(self.window, message);
-		if (!result) {
-			self.record('chooseCancelOnNextConfirmation', null, null, true);
-		}
+    };
+    window.confirm = function(message) {
+        var result = self.windowMethods.confirm.call(self.window, message);
+        if (!result) {
+            self.record('chooseCancelOnNextConfirmation', null, null, true);
+        }
         self.record('assertConfirmation', message);
-		return result;
-	};
-	window.prompt = function(message) {
-		var result = self.windowMethods.prompt.call(self.window, message);
-		self.record('answerOnNextPrompt', result, null, true);
+        return result;
+    };
+    window.prompt = function(message) {
+        var result = self.windowMethods.prompt.call(self.window, message);
+        self.record('answerOnNextPrompt', result, null, true);
         self.record('assertPrompt', message);
-		return result;
-	};
-	window.open = function (url, windowName, windowFeatures, replaceFlag) {
-		if (self.openCalled) {
-			// stop the recursion called by modifyWindowToRecordPopUpDialogs
-			return self.originalOpen.call(window, url, windowName, windowFeatures, replaceFlag);
-		} else {
-			self.openCalled = true;
-			var result = self.windowMethods.open.call(window, url, windowName, windowFeatures, replaceFlag);
-			self.openCalled = false;
+        return result;
+    };
+    window.open = function (url, windowName, windowFeatures, replaceFlag) {
+        if (self.openCalled) {
+            // stop the recursion called by modifyWindowToRecordPopUpDialogs
+            return self.originalOpen.call(window, url, windowName, windowFeatures, replaceFlag);
+        } else {
+            self.openCalled = true;
+            var result = self.windowMethods.open.call(window, url, windowName, windowFeatures, replaceFlag);
+            self.openCalled = false;
             if (result.wrappedJSObject) {
                 result = result.wrappedJSObject;
             }
-			setTimeout(Recorder.record, 0, self, 'waitForPopUp', windowName, "30000");
-			return result;
-		}
-	};
+            setTimeout(Recorder.record, 0, self, 'waitForPopUp', windowName, "30000");
+            return result;
+        }
+    };
 };
 
 Recorder.prototype.parseEventKey = function(eventKey) {
-	if (eventKey.match(/^C_/)) {
-		return { eventName: eventKey.substring(2), capture: true };
-	} else {
-		return { eventName: eventKey, capture: false };
-	}
+    if (eventKey.match(/^C_/)) {
+        return { eventName: eventKey.substring(2), capture: true };
+    } else {
+        return { eventName: eventKey, capture: false };
+    }
 };
 
 Recorder.prototype.attach = function() {
     console.log("attaching");
-	this.locatorBuilders = new LocatorBuilders(window);
-	this.eventListeners = {};
-	this.reattachWindowMethods();
-	var self = this;
-	for (var eventKey in Recorder.eventHandlers) {
-		var eventInfo = this.parseEventKey(eventKey);
-		var eventName = eventInfo.eventName;
-		var capture = eventInfo.capture;
-		// create new function so that the variables have new scope.
-		function register() { // jshint ignore:line
-			var handlers = Recorder.eventHandlers[eventKey];
-			//console.log('eventName=' + eventName + ' / handlers.length=' + handlers.length);
-			var listener = function(event) {
-				for (var i = 0; i < handlers.length; i++) {
+    this.locatorBuilders = new LocatorBuilders(window);
+    this.eventListeners = {};
+    this.reattachWindowMethods();
+    var self = this;
+    for (var eventKey in Recorder.eventHandlers) {
+        var eventInfo = this.parseEventKey(eventKey);
+        var eventName = eventInfo.eventName;
+        var capture = eventInfo.capture;
+        // create new function so that the variables have new scope.
+        function register() { // jshint ignore:line
+            var handlers = Recorder.eventHandlers[eventKey];
+            //console.log('eventName=' + eventName + ' / handlers.length=' + handlers.length);
+            var listener = function(event) {
+                for (var i = 0; i < handlers.length; i++) {
                     handlers[i].call(self, event);
-				}
-			};
-			if (browserVersion.isIE && browserVersion.ieMode < 11) {
-			    window.document.attachEvent("on" + eventName, listener);
-			} else {
-			    window.document.addEventListener(eventName, listener, capture);
-			}
-			this.eventListeners[eventKey] = listener;
-		}
-		register.call(this);
-	}
+                }
+            };
+            if (browserVersion.isIE && browserVersion.ieMode < 11) {
+                window.document.attachEvent("on" + eventName, listener);
+            } else {
+                window.document.addEventListener(eventName, listener, capture);
+            }
+            this.eventListeners[eventKey] = listener;
+        }
+        register.call(this);
+    }
 
-	var lw = JSON.stringify(new LastWindow(window));
+    var lw = JSON.stringify(new LastWindow(window));
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST", Recorder.GetIdeUrl() + "/lastwin_store");
     xmlhttp.onreadystatechange = function() {
@@ -128,7 +128,7 @@ Recorder.prototype.attach = function() {
     };
     xmlhttp.send(lw);
 
-	Recorder.cmdSend('open', document.URL, null, (new Date()).getTime());
+    Recorder.cmdSend('open', document.URL, null, (new Date()).getTime());
 };
 
 Recorder.record = function (recorder, command, target, value) {
@@ -219,7 +219,7 @@ function createPath(window) {
 }
 
 Recorder.prototype.findLocator = function (element) {
-	return this.locatorBuilders.build(element);
+    return this.locatorBuilders.build(element);
 };
 
 Recorder.prototype.findLocators = function (element) {
@@ -229,25 +229,25 @@ Recorder.prototype.findLocators = function (element) {
 };
 
 Recorder.addEventHandler = function(handlerName, eventName, handler, options) {
-	handler.handlerName = handlerName;
-	if (!options) options = {};
-	var key = options.capture ? ('C_' + eventName) : eventName;
-	if (!this.eventHandlers[key]) {
-		this.eventHandlers[key] = [];
-	}
-	this.eventHandlers[key].push(handler);
+    handler.handlerName = handlerName;
+    if (!options) options = {};
+    var key = options.capture ? ('C_' + eventName) : eventName;
+    if (!this.eventHandlers[key]) {
+        this.eventHandlers[key] = [];
+    }
+    this.eventHandlers[key].push(handler);
 };
 
 Recorder.removeEventHandler = function(handlerName) {
-	for (var eventKey in this.eventHandlers) {
-		var handlers = this.eventHandlers[eventKey];
-		for (var i = 0; i < handlers.length; i++) {
-			if (handlers[i].handlerName == handlerName) {
-				handlers.splice(i, 1);
-				break;
-			}
-		}
-	}
+    for (var eventKey in this.eventHandlers) {
+        var handlers = this.eventHandlers[eventKey];
+        for (var i = 0; i < handlers.length; i++) {
+            if (handlers[i].handlerName == handlerName) {
+                handlers.splice(i, 1);
+                break;
+            }
+        }
+    }
 };
 
 Recorder.eventHandlers = {};
@@ -302,12 +302,12 @@ Recorder.isSameWindow = function (lastwin, window) {
 
 Recorder.inputTypes = ["text", "password", "file", "datetime", "datetime-local", "date", "month", "time", "week", "number", "range", "email", "url", "search", "tel", "color"];
 Recorder.addEventHandler('type', 'change', function (ev, check_prev) {
-	console.log('event handler: type, change');
-    var ev = ev || window.event;				// IE 9 quirks mode fix. event is global. also srcElement instead of target.
+    console.log('event handler: type, change');
+    var ev = ev || window.event;                // IE 9 quirks mode fix. event is global. also srcElement instead of target.
     var target = ev.target || ev.srcElement;
-	if (check_prev && target.value == this.activeElementValue) {	// value hasn't changed
-		return;
-	}
+    if (check_prev && target.value == this.activeElementValue) {    // value hasn't changed
+        return;
+    }
     if (target.tagName) {
         var tagName = target.tagName.toLowerCase();
         var type = target.type;
@@ -432,9 +432,9 @@ Recorder.addEventHandler('select', 'change', function(ev, check_prev) {
         if ('select' == tagName) {
             if (!target.multiple) {
                 var option = target.options[target.selectedIndex];
-				if (check_prev && this.activeElementValue == option) {	// value did not change
-					return;
-				}
+                if (check_prev && this.activeElementValue == option) {    // value did not change
+                    return;
+                }
                 console.log('selectedIndex=' + target.selectedIndex);
                 this.record("select", this.findLocators(target), this.getOptionLocator(option));
             } else {
@@ -457,7 +457,7 @@ Recorder.addEventHandler('select', 'change', function(ev, check_prev) {
                 }
             }
         }
-	}
+    }
 });
 
 Recorder.addEventHandler('clickLocator', 'click', function (ev) {
@@ -477,13 +477,13 @@ Recorder.addEventHandler('clickLocator', 'click', function (ev) {
 }, { capture: true });
 
 Recorder.prototype.findClickableElement = function (e) {
-	//console.log('finding clickable...');
+    //console.log('finding clickable...');
     if (!e.tagName) return null;
     var tagName = e.tagName.toLowerCase();
     var type = e.type;
     if (e.getAttribute("onclick") !== null || e.getAttribute("href") !== null || tagName == "button" ||
-		(tagName == "input" &&
-		 (type == "submit" || type == "button" || type == "image" || type == "radio" || type == "checkbox" || type == "reset"))) {
+        (tagName == "input" &&
+         (type == "submit" || type == "button" || type == "image" || type == "radio" || type == "checkbox" || type == "reset"))) {
         return e;
     } else {
         if (e.parentNode !== null && e.parentNode !== undefined) {
