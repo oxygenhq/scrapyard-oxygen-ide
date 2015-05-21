a42d7c109efa0cd6c49937a6e23fe519cf81476f
romovs <romovs@gmail.com>
2015-05-21 12:13:57 +0300

    Bump Electron to 0.26.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         'download-electron': {
-            version: '0.26.0',
+            version: '0.26.1',
             outputDir: OUTDIR,
             rebuild: false
         },
