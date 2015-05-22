ffaf041689a979d15673769c225c990e0113ea1a
romovs <romovs@gmail.com>
2015-05-22 19:21:40 +0300

    Renamed assert.throw to assert.fail

--- src/module-assert.js
+++ src/module-assert.js
@@ -2,6 +2,6 @@ module.exports = function(execMethod) {
     var module = {};
     module.equal = function() { return execMethod('assert', 'equal', Array.prototype.slice.call(arguments)); };
     module.notEqual = function() { return execMethod('assert', 'notEqual', Array.prototype.slice.call(arguments)); };
-    module.assert = function() { return execMethod('assert', 'assert', Array.prototype.slice.call(arguments)); };
+    module.fail = function() { return execMethod('assert', 'fail', Array.prototype.slice.call(arguments)); };
     return module;
 };
