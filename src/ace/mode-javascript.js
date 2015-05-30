23e1d78448b9298f490713ebd27cc43e8746377f
romovs <romovs@gmail.com>
2015-05-31 01:02:21 +0300

    Fixed syntax highlighting yet again

--- src/ace/mode-javascript.js
+++ src/ace/mode-javascript.js
@@ -88,7 +88,7 @@ var JavaScriptHighlightRules = function(options) {
     var kwBeforeRe = "case|do|else|finally|in|instanceof|return|throw|try|typeof|yield|void";
 
     // TODO: Unicode escape sequences
-    var identifierRe = "[a-zA-Z\\$_\u00a1-\uffff][a-zA-Z\\d\\$_\u00a1-\uffff]*\\b";
+    var identifierRe = "[.a-zA-Z\\$_\u00a1-\uffff][a-zA-Z\\d\\$_\u00a1-\uffff]*\\b";
 
     var escapedRe = "\\\\(?:x[0-9a-fA-F]{2}|" + // hex
         "u[0-9a-fA-F]{4}|" + // unicode
