520efa71ea5ce7a7369579f3fd00fce22f06dc3f
romovs <romovs@gmail.com>
2015-07-15 22:17:36 +0300

    Updated web module docs

--- src/module-web.js
+++ src/module-web.js
@@ -15,8 +15,8 @@
  *  <li><code>exact:STRING</code> - Match the string verbatim.</li>
  *  <li><code>glob:PATTERN</code> - Match using case-insensitive glob pattern. 
  *      <code>?</code> will match any single character except new line (\n).
- *      <code>*</code> will match any sequence of characters except new line. Empty PATTERN will 
- *      match only other empty strings.</li>
+ *      <code>*</code> will match any sequence (0 or more) of characters except new line. Empty 
+ *      PATTERN will match only other empty strings.</li>
  *  <li><code>PATTERN</code> - Same as glob matching.</li>
  *  </ul>
  * </div>
