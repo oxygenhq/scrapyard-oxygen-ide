297d80ad7d44bdf31df8282a913de9e6ebd8a89b
romovs <romovs@gmail.com>
2017-04-10 00:59:01 +0300

    [installer-win] Migrate to WiX

--- tools/grunt-tasks/installer-win-task.js
+++ tools/grunt-tasks/installer-win-task.js
@@ -1,19 +1,71 @@
 var cp = require('child_process');
-var path = require('path');
 var os = require('os');
 
 module.exports = function(grunt) {
     grunt.registerTask('installer-win', 'Creates setup package for the Windows platform.', function() {
         if (os.platform() === 'win32') {
             var cfg = grunt.config.get('installer-win');
-            var nsiPath = path.resolve(__dirname, '..', 'installers', 'win', 'oxygen.nsi');
-
-            var child = cp.execFileSync('makensis', 
-                                        [ '/DPRODUCT_VERSION=' + cfg.version, 
-                                          '/DPRODUCT_ARCH=' + cfg.arch,
-                                          '/WX', nsiPath],
-                                        { stdio : 'inherit'});
+            var wixRoot = 'tools\\installer-win\\';
+            var ieAddonRoot = 'browser-extensions\\ie\\bin\\Release';
+            var version = cfg.version;
+            var arch = cfg.arch === 'x64' ? 'x64' : 'x86';
+            
+            cp.execFileSync('heat', 
+                            [ 'file', ieAddonRoot + '\\IEAddon.dll',
+                              '-srd',
+                              '-gg',
+                              '-cg', 'IEAddonDLL',
+                              '-out', wixRoot + 'ie_addon.wxs'],
+                            { stdio : 'inherit'});
+                                        
+            cp.execFileSync('heat', 
+                            [ 'dir', 'build',
+                              '-o', wixRoot + 'files.wxs',
+                              '-scom',
+                              '-frag',
+                              '-srd',
+                              '-sreg',
+                              '-gg',
+                              '-cg', 'ApplicationFiles',
+                              '-dr', 'INSTALLFOLDER',
+                              '-t', wixRoot + 'files.xslt'],
+                            { stdio : 'inherit'});
+                            
+            cp.execFileSync('candle', 
+                            [ '-arch', arch,
+                              '-dVersion=' + version,
+                              '-ext', 'WixFirewallExtension',
+                              '-o', wixRoot + 'config.wixobj',
+                              wixRoot + 'config.wxs'],
+                            { stdio : 'inherit'});
+                            
+            cp.execFileSync('candle', 
+                            [ '-arch', arch,
+                              '-ext', 'WixFirewallExtension',
+                              '-o', wixRoot + 'files.wixobj',
+                              wixRoot + 'files.wxs'],
+                            { stdio : 'inherit'});
+                            
+            cp.execFileSync('candle', 
+                            [ '-arch', arch,
+                              '-ext', 'WixFirewallExtension',
+                              '-o', wixRoot + 'ie_addon.wixobj',
+                              wixRoot + 'ie_addon.wxs'],
+                            { stdio : 'inherit'});
+                            
+            cp.execFileSync('light', 
+                            [ '-ext', 'WixNetFxExtension',
+                              '-ext', 'WixUIExtension',
+                              '-ext', 'WixFirewallExtension',
+                              '-ext', 'WixUtilExtension',
+                              '-spdb',
+                              '-b', 'build',
+                              '-b', ieAddonRoot,
+                              '-o', 'dist\\oxygen-ide-' + version + '-' + cfg.arch + '.msi',
+                              wixRoot + 'config.wixobj',
+                              wixRoot + 'files.wixobj',
+                              wixRoot + 'ie_addon.wixobj'],
+                            { stdio : 'inherit'});
         }
     });
 };
-
