9aac7ed21ce3d4a37b0362557d7aafd51e002e82
romovs <romovs@gmail.com>
2017-07-09 00:44:09 +0300

    Bump selenium

--- oxygen-cli/win/selenium.cmd
+++ oxygen-cli/win/selenium.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 set "selpath=%~dp0\..\selenium"
-java -Dwebdriver.chrome.driver="%selpath%\chromedriver.exe" -Dwebdriver.ie.driver="%selpath%\IEDriverServer_x86.exe" -jar "%selpath%\selenium-server-standalone-3.0.1.jar" -port 4444
+java -Dwebdriver.chrome.driver="%selpath%\chromedriver.exe" -Dwebdriver.ie.driver="%selpath%\IEDriverServer_x86.exe" -jar "%selpath%\selenium-server-standalone-3.4.0.jar" -port 4444
